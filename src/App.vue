8c41a1c30ed04476bbb8bc5be3f307cca37e468c
wangwenjie <1772755454@qq.com>
2020-05-14 10:08:42 +0800

    添加列表滚动

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <VirtualList :data="list"></VirtualList>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
+import VirtualList from "./components/VirtualList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    // HelloWorld,
+    VirtualList
+  },
+  data() {
+    return {
+      list: []
+    };
+  },
+  created(){
+    this.listFn()
+  },
+  methods:{
+    listFn(){
+      for(let i=0;i<100;i++){
+        this.list.push({id:i,value:'Item'+i})
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
